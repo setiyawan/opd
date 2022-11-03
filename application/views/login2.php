a20882a96c7ff89a26e644c441e2b4388d6f40b1
setiyawan <setiyawan1994@gmail.com>
2022-11-03 19:35:32 +0700

    update path

--- application/views/login2.php
+++ application/views/login2.php
@@ -17,7 +17,7 @@
   class="light-style customizer-hide"
   dir="ltr"
   data-theme="theme-default"
-  data-assets-path="<?= base_url()?>/asset/"
+  data-assets-path="<?= base_url()?>asset/"
   data-template="vertical-menu-template-free"
 >
   <head>
@@ -35,37 +35,37 @@
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url()?>asset/images/favicon.png">
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com" /> -->
+    <!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> -->
+    <!-- <link
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
       rel="stylesheet"
-    />
+    /> -->
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="<?= base_url()?>/asset/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="<?= base_url()?>asset/vendor/fonts/boxicons.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="<?= base_url()?>/asset/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?= base_url()?>/asset/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="<?= base_url()?>/asset/css/demo.css" />
+    <link rel="stylesheet" href="<?= base_url()?>asset/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?= base_url()?>asset/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="<?= base_url()?>asset/css/demo.css" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="<?= base_url()?>/asset/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="<?= base_url()?>asset/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
     <!-- Page CSS -->
     <!-- Page -->
-    <link rel="stylesheet" href="<?= base_url()?>/asset/vendor/css/pages/page-auth.css" />
+    <link rel="stylesheet" href="<?= base_url()?>asset/vendor/css/pages/page-auth.css" />
     <!-- Helpers -->
-    <script src="<?= base_url()?>/asset/vendor/js/helpers.js"></script>
+    <script src="<?= base_url()?>asset/vendor/js/helpers.js"></script>
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?= base_url()?>/asset/js/config.js"></script>
+    <script src="<?= base_url()?>asset/js/config.js"></script>
 
     <style type="text/css">
       body {
-        background-image: url(<?= base_url()?>/asset/images/bkad.jpg);
+        background-image: url(<?= base_url()?>asset/images/bkad.jpg);
         /*background-position-y: -120px;*/
         /*background-repeat: no-repeat;*/
         background-size: cover;
@@ -148,22 +148,22 @@
 
     <!-- Core JS -->
     <!-- build:js asset/vendor/js/core.js -->
-    <script src="<?= base_url()?>/asset/vendor/libs/jquery/jquery.js"></script>
-    <script src="<?= base_url()?>/asset/vendor/libs/popper/popper.js"></script>
-    <script src="<?= base_url()?>/asset/vendor/js/bootstrap.js"></script>
-    <script src="<?= base_url()?>/asset/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="<?= base_url()?>asset/vendor/libs/jquery/jquery.js"></script>
+    <script src="<?= base_url()?>asset/vendor/libs/popper/popper.js"></script>
+    <script src="<?= base_url()?>asset/vendor/js/bootstrap.js"></script>
+    <script src="<?= base_url()?>asset/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-    <script src="<?= base_url()?>/asset/vendor/js/menu.js"></script>
+    <script src="<?= base_url()?>asset/vendor/js/menu.js"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
 
     <!-- Main JS -->
-    <script src="<?= base_url()?>/asset/js/main.js"></script>
+    <script src="<?= base_url()?>asset/js/main.js"></script>
 
     <!-- Page JS -->
 
     <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- <script async defer src="https://buttons.github.io/buttons.js"></script> -->
   </body>
 </html>
